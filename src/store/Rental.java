641e0866b2def6f64e0fca09599ec02a10da4677
Zanella-dev <krzanela@minha.fag.edu.br>
2025-11-20 13:14:42 -0300

    Primeiro commit

--- src/store/Rental.java
+++ src/store/Rental.java
@@ -2,8 +2,19 @@ package store;
 
 public class Rental {
 
-	public Rental() {
-		// TODO Auto-generated constructor stub
-	}
+	   private Movie _movie;
+	   private int _daysRented;
 
-}
+	   public Rental(Movie movie, int daysRented) {
+	      _movie = movie;
+	      _daysRented = daysRented;
+	   }
+
+	   public int getDaysRented() {
+	      return _daysRented;
+	   }
+
+	   public Movie getMovie() {
+	      return _movie;
+	   }
+	}
