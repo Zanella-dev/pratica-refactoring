18ff4defc92661394c9412c3aa7c3d500b1b14e4
Zanella-dev <krzanela@minha.fag.edu.br>
2025-11-22 10:18:45 -0300

    Terceiro commit

--- src/store/Customer.java
+++ src/store/Customer.java
@@ -1,3 +1,5 @@
+package store;
+
 import java.util.Enumeration;
 import java.util.Vector;
 
@@ -49,21 +51,21 @@ public class Customer {
    }
 
    // ESTE É O MÉTODO NOVO EXTRAÍDO (Extract Method)
-   private double amountFor(Rental each) {
+   private double amountFor(Rental aRental) {
       double thisAmount = 0;
-      switch (each.getMovie().getPriceCode()) {
+      switch (aRental.getMovie().getPriceCode()) {
          case Movie.REGULAR:
             thisAmount += 2;
-            if (each.getDaysRented() > 2)
-               thisAmount += (each.getDaysRented() - 2) * 1.5;
+            if (aRental.getDaysRented() > 2)
+               thisAmount += (aRental.getDaysRented() - 2) * 1.5;
             break;
          case Movie.NEW_RELEASE:
-            thisAmount += each.getDaysRented() * 3;
+            thisAmount += aRental.getDaysRented() * 3;
             break;
          case Movie.CHILDRENS:
             thisAmount += 1.5;
-            if (each.getDaysRented() > 3)
-               thisAmount += (each.getDaysRented() - 3) * 1.5;
+            if (aRental.getDaysRented() > 3)
+               thisAmount += (aRental.getDaysRented() - 3) * 1.5;
              break;
       }
       return thisAmount;
